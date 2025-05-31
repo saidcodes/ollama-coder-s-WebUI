07a4d892d19c97b746303881b9a0c25ff12ffc93
Said ID Mohammed <saidsee.me@gmail.com>
2025-05-31 18:15:38 +0200

    refine:

--- index.tsx
+++ index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { OllamaProvider } from './contexts/OllamaContext';
 
+
+
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
