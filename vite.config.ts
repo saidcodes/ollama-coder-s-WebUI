ca5fef03deae32522380ef325a5cb68a7a5319d4
Said ID Mohammed <saidsee.me@gmail.com>
2025-05-31 18:17:03 +0200

    tailwind import

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig(({ mode }) => {
+  
     const env = loadEnv(mode, '.', '');
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+      plugins: [
+    tailwindcss(),
+  ],
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
       }
+      
     };
 });
+
