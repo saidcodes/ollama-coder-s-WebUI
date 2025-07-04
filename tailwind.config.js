5a0eebc578d8caaaed0c4a002257396960eab588
Said ID Mohammed <saidsee.me@gmail.com>
2025-07-05 00:16:05 +0200

    taillwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
-module.exports = {
-  // ...existing config...
-  theme: {
-    extend: {
-      colors: {
-        neutral: {
-          850: '#1a1a1a',
-          900: '#171717',
-        }
+export const theme = {
+  extend: {
+    colors: {
+      neutral: {
+        850: '#1a1a1a',
+        900: '#171717',
       }
     }
-  }
-}
+  } 
+};
